6472a20d37c88798e597955adfb5665b7ffb0178
Ian Sein <ianseinmichel@gmail.com>
2021-11-22 09:52:36 -0300

    Recuperatorio TP1

---
+++ RecuperatoriosTP/Trabajo Practico Nº 1/Entidades/Operando.cs
@@ -0,0 +1,241 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        #region Constructores
+
+        /// <summary>
+        /// Inicializa el numero en 0.
+        /// </summary>
+        public Operando() : this(0)
+        {
+        }
+
+        /// <summary>
+        /// Inicializa el numero con el valor pasado por parametro.
+        /// </summary>
+        /// <param name="numero"> Valor para inicializar el numero </param> 
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        /// <summary>
+        /// Convierte el numero en formato de string a un formato double.
+        /// </summary>
+        /// <param name="strNumero"> Numero con formato string a convertir y setear. </param> 
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        
+        /// <summary>
+        /// Valida el numero y lo setea.
+        /// </summary>
+        public string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        #endregion
+
+        #region Validacion
+
+        /// <summary>
+        /// Intenta comprobar si lo recibido por parametro es numerico.
+        /// </summary>
+        /// <param name="numero"> String que va a ser convertido en double. </param>
+        /// <returns> El numero en formato double si es correcto, sino 0 </returns>
+        private double ValidarOperando(string numero)
+        {
+            double auxNumero;
+
+            if(!double.TryParse(numero, out auxNumero))
+            {
+                auxNumero = 0;
+            }
+
+            return auxNumero;
+        }
+
+        /// <summary>
+        /// Chequea que el string no contenga ni '1' ni '0'.
+        /// </summary>
+        /// <param name="binario"> String a validar. </param>
+        /// <returns> True si el string esta en formato binario o False si no.</returns>
+        private bool EsBinario(string binario)
+        {
+            for (int i = 0; i < binario.Length; i++)
+            {
+                if(binario[i] != '1' && binario[i] != '0')
+                {
+                    return false;
+                }
+               
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Convertidores
+
+        /// <summary>
+        /// Convierte el binario con formato string en un numero decimal.
+        /// </summary>
+        /// <param name="binario"> String a ser convertido en decimal</param>
+        /// <returns> El decimal con formato string si es correcto o "Valor invalido" si no. </returns>
+        public string BinarioDecimal(string binario)
+        {
+            char[] arrayBinario = binario.ToCharArray();
+            
+            Array.Reverse(arrayBinario);
+
+            int decimalTotal = 0;
+            
+            if(EsBinario(binario))
+            {
+                for (int i = 0; i < arrayBinario.Length; i++)
+                {
+                    if (arrayBinario[i] == '1')
+                    {
+                        decimalTotal += (int)Math.Pow(2, i);
+                    }
+
+                }
+
+                return decimalTotal.ToString();
+            }
+
+            return "Valor invalido\n";
+        }
+
+        /// <summary>
+        /// Convierte un numero decimal a un numero binario en formato string
+        /// </summary>
+        /// <param name="numero"> Numero a convertir a formato binario </param>
+        /// <returns> El numero binario en formato de string </returns>
+        public string DecimalBinario(double numero)
+        {
+            string strBinario = string.Empty;
+
+            if(numero < 0)
+            {
+                strBinario = "-1";
+                return strBinario;
+            }
+
+            if (numero == 0)
+            {
+                strBinario = "0";
+            }
+            else
+            {
+                while (numero > 0)
+                {
+                    strBinario = (int)numero % 2 + strBinario;
+                    numero = (int)numero / 2;
+                }
+
+                return strBinario;
+            }
+
+            return strBinario;
+        }
+
+        /// <summary>
+        /// Convierte un numero en formato string a un numero binario en formato de string
+        /// </summary>
+        /// <param name="numero"> Numero en formato de string a ser convertido </param>
+        /// <returns> Numero Binario en formato de string si es correcto o "Valor invalido" si no </returns>
+        public string DecimalBinario(string numero)
+        {
+            double auxNumero;
+            string binario;
+
+            if(Double.TryParse(numero, out auxNumero))
+            {
+                binario = DecimalBinario(auxNumero);
+
+                if(binario != "-1")
+                {
+                    return binario;
+                }
+
+            }
+
+            return "Valor invalido";
+            
+            
+        }
+
+        #endregion
+
+        #region Operadores
+
+        /// <summary>
+        /// Hace una suma entre 2 numeros.
+        /// </summary>
+        /// <param name="num1">Primer numero para sumar. </param>
+        /// <param name="num2">Segundo numero para sumar. </param>
+        /// <returns> La suma de los 2 numeros. </returns>
+        public static double operator +(Operando num1, Operando num2)
+        {
+            return num1.numero + num2.numero;
+        }
+
+        /// <summary>
+        /// Hace una resta entre 2 numeros.
+        /// </summary>
+        /// <param name="num1">Primer numero para restar.</param>
+        /// <param name="num2">Segundo numero para restar.</param>
+        /// <returns> La resta de los 2 numeros. </returns>
+        public static double operator -(Operando num1, Operando num2)
+        {
+            return num1.numero - num2.numero;
+        }
+
+        /// <summary>
+        /// Hace una multiplicacion entre 2 numeros.
+        /// </summary>
+        /// <param name="num1">Primer numero para multiplicar.</param>
+        /// <param name="num2">Segundo numero para multiplicar.</param>
+        /// <returns> La multiplicacion de los 2 numeros. </returns>
+        public static double operator *(Operando num1, Operando num2)
+        {
+            return num1.numero * num2.numero;
+        }
+
+        /// <summary>
+        /// Hace una division entre 2 numeros.
+        /// </summary>
+        /// <param name="num1"> Primer numero para dividir. </param>
+        /// <param name="num2"> Segundo numero para dividir.</param>
+        /// <returns> La division de los 2 numeros, pero si el segundo numero es 0 retorna el minimo valor de un double</returns>
+        public static double operator /(Operando num1, Operando num2)
+        {
+            if (num2.numero != 0)
+            {
+                return num1.numero / num2.numero;
+            }
+
+            return Double.MinValue;
+        }
+
+        #endregion
+
+    }
+}
---
+++ RecuperatoriosTP/Trabajo Practico Nº 1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        #region Limpiar Formulario
+
+        /// <summary>
+        /// Limpia el formulario.
+        /// </summary>
+        private void Limpiar()
+        {
+            this.txtNumero1.Text = null;
+            this.txtNumero2.Text = null;
+            this.cmbOperador.Text = null;
+            this.lblResultado.Text = null;
+        }
+
+        /// <summary>
+        /// Limpia el formulario al apretar el boton "LIMPIAR"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        #endregion
+
+        #region Cerrar Formulario
+
+        /// <summary>
+        /// Pregunta si se quiere salir de la aplicacion, y lo hace depende la decision.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+
+            this.Close();
+            
+        }
+
+        /// <summary>
+        ///  Pregunta si se quiere salir de la aplicacion, y lo hace depende la decision.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro de querer salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #endregion
+
+        #region Operar
+
+        /// <summary>
+        /// Realiza una operacion, la muestra en el label de "Resultado" y la operacion hecha queda guardada en un historial.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+
+            this.lblResultado.Text = resultado.ToString();
+            
+            string historialOperacion = txtNumero1.Text + " " + cmbOperador.Text + " " + txtNumero2.Text + " " + "=" + " " + resultado;
+
+            this.lstOperaciones.Items.Add(historialOperacion);
+            
+
+        }
+
+        /// <summary>
+        /// Realiza una operacion y calcula el resultado.
+        /// </summary>
+        /// <param name="numero1"> Primer numero escrito por el usuario </param>
+        /// <param name="numero2"> Segundo numero escrito por el usuario </param>
+        /// <param name="operador"> Operador elegido por el usuario </param>
+        /// <returns> Resultado de la operacion </returns>
+        static private double Operar(string numero1, string numero2, string operador)
+        {
+            double resultado = 0;
+            char operadorValido;
+
+            char.TryParse(operador, out operadorValido);
+
+            Operando numero1Convertido = new Operando(numero1);
+            Operando numero2Convertido = new Operando(numero2);
+
+            resultado = Calculadora.Operar(numero1Convertido, numero2Convertido, operadorValido);
+
+                
+            return resultado;
+        }
+
+        #endregion
+
+        #region Convertidores
+
+        /// <summary>
+        /// Realiza la conversion a decimal de un numero y lo muestra en el cuadro de resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Operando numeroAConvertir = new Operando();
+
+            string resultado = numeroAConvertir.BinarioDecimal(lblResultado.Text);
+
+            lblResultado.Text = resultado;
+        }
+
+        /// <summary>
+        /// Realiza la conversion a binario de un numero decimal y lo muestra en el cuadro de resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+
+            Operando numeroAConvertir = new Operando();
+
+            string resultado = numeroAConvertir.DecimalBinario(lblResultado.Text);
+
+            lblResultado.Text = resultado;
+
+        }
+
+        #endregion
+
+    }
+}
